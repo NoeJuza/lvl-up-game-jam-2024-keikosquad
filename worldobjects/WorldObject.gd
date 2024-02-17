f77f7a0c128b3a17b01aeb005838380f992d831d
noe.juzan <noe.juzan.pro@gmail.com>
2024-02-17 17:42:47 +0100

    Xtests

--- worldobjects/WorldObject.gd
+++ worldobjects/WorldObject.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 class_name WorldObject
 @export var type: String = ""
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
