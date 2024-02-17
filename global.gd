6993da7079ce5a83f93c55eef9682406fe558fff
Jean-Nicolas Thurre <jean-nicolas.thurre@edu.hefr.ch>
2024-02-17 23:26:17 +0100

    Update global.gd

--- global.gd
+++ global.gd
@@ -7,14 +7,17 @@ var material_array = [
 	{"name": "wood", "amount": 0}
 ]
 
-# Setter pour la valeur "amount" dans le tableau material_array
+# Setter amount
 func set_material_amount(name, value):
 	material_array[material_array.find(name)]["amount"] += value
 	emit_signal("material_amount_changed", name, value)
-	print("emit")
-	
 
-# Getter pour la valeur "amount" dans le tableau material_array
+# Add amount (TO KEEP)
+func add_material_amount(name, value):
+	material_array[material_array.find(name)]["amount"] += value
+	emit_signal("material_amount_changed", name, value)
+	
+# Getter amount
 func get_material_amount(name):
 	return material_array[material_array.find(name)]["amount"]
 
