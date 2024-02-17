b0437629d613261225a0c20bbea6b0d14761c35c
noe.juzan <noe.juzan.pro@gmail.com>
2024-02-17 23:26:48 +0100

    wip: character behavior

--- players/player2/player2.gd
+++ players/player2/player2.gd
@@ -8,8 +8,8 @@ const SPEED = 250
 var is_dropping: bool = false
 var is_holding : bool = false
 var held_item : WorldObject = null
-var has_item_near = false
-var last_entered_item_array : Array[WorldObject] = []
+var has_interactible_near = false
+var interactible_queue : Array[WorldObject] = []
 
 func _convert_to_isometric(cartesian):
 	var screen_pos = Vector2()
@@ -54,12 +54,15 @@ func _physics_process(delta):
 	move_and_slide()
 	
 	if is_holding:
-		var offset
-		if not $AnimatedSprite2d.flip_h: 
-			offset = _convert_to_isometric(Vector2(0,-80))
+		if held_item == null:
+			is_holding == false
 		else:
-			offset = _convert_to_isometric(Vector2(-80,0))
-		held_item.position = position + get_parent().position + offset
+			var offset
+			if not $AnimatedSprite2d.flip_h: 
+				offset = _convert_to_isometric(Vector2(0,-80))
+			else:
+				offset = _convert_to_isometric(Vector2(-80,0))
+			held_item.position = position + get_parent().position + offset
 		
 func _on_interaction_zone_ready():
 	print("interaction zone ready")
@@ -69,29 +72,41 @@ func _on_interaction_zone_body_shape_entered(body_rid, body, body_shape_index, l
 	var parentNode = body.get_parent()
 	if parentNode is WorldObject:
 		print(parentNode.type)
-		has_item_near = true
-		last_entered_item_array.append(parentNode)
+		has_interactible_near = true
+		interactible_queue.append(parentNode)
 		$interaction_tooltip.show()
 		$interaction_tooltip.play()
 
 func _on_interaction_zone_body_shape_exited(body_rid, body, body_shape_index, local_shape_index):
 	print("body has left zone")
-	var parentNode = body.get_parent()
-	if parentNode is WorldObject:
-		var index = last_entered_item_array.find(parentNode)
-		last_entered_item_array.remove_at(index)
-		if last_entered_item_array.size() == 0:
-			has_item_near = false
-			$interaction_tooltip.hide()
-			$interaction_tooltip.stop()
+	if not body == null:
+		var parentNode = body.get_parent()
+		if parentNode is WorldObject:
+			var index = interactible_queue.find(parentNode)
+			interactible_queue.remove_at(index)
+			if interactible_queue.size() == 0:
+				has_interactible_near = false
+				$interaction_tooltip.hide()
+				$interaction_tooltip.stop()
 
 func _input(ev):
 	if Input.is_action_just_pressed("interact"):
 		if is_holding :
+			for interactible in interactible_queue:
+				if interactible.type == "construction" and held_item.type == "component":
+					if interactible.hps < 100:
+						held_item.queue_free()
+						interactible.heal()
+						break
+				if interactible.type == "dropper" and held_item.type == "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           "
 			is_holding = false
 			held_item = null
 			is_dropping = true
-		elif has_item_near and not is_dropping:
-			is_holding = true
-			held_item = last_entered_item_array[0]
+		elif has_interactible_near and not is_dropping: # pickup item
+			for interactible in interactible_queue:
+				if interactible.type == "material" or interactible.type == "component":
+					is_holding = true
+					held_item = interactible
+					break
+		
 	is_dropping = false
--- worldobjects/battle/construction/construction.gd
+++ worldobjects/battle/construction/construction.gd
@@ -1,11 +1,36 @@
 extends WorldObject
-
-
+class_name Construction
+var hps = 100
+var has_already_lost_half = false
+signal hps_changed
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ProgressBar.value = hps
 
-
+func heal():
+	hps += 25
+	hps_changed.emit()
+	
+func damage(amount):
+	if hps > 0:
+		hps -= amount
+		hps_changed.emit()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_hps_changed():
+	$ProgressBar.value = hps
+	if hps <= 50 and not has_already_lost_half:
+		has_already_lost_half = true
+		create_component_in_front()
+	if hps <= 0:
+		create_component_in_front()
+		queue_free()
+func create_component_in_front():
+	var component = load("res://worldobjects/battle/component/component.tscn")
+	var instance = component.instantiate()
+	get_parent().add_child(instance)
+	instance.position = Vector2(position.x, position.y + 100)
