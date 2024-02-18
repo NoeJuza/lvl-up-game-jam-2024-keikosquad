f2ea2648442e1803dcd70e4c75e37ddeea9ae8a0
Jean-Nicolas Thurre <jean-nicolas.thurre@edu.hefr.ch>
2024-02-18 04:22:44 +0100

    ennemy: Animation add

--- ennemies/ennemies_character.gd
+++ ennemies/ennemies_character.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 300
+@export var speed = 150
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+
+func _physics_process(delta):
+	$AnimatedSprite.play("walking")
+	
