0da4affe5a2b659554428cdd90ccd38463e0559c
Rhombicdodecahedron <velsac.123@gmail.com>
2024-02-18 13:03:49 +0100

    Implement delete tower logic

--- global.gd
+++ global.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal stock_amount_changed()
 signal item_selected_buyzone(index)
+signal remove_placed_tile(event)
 var materials = {"egg":{"name":"egg","texture": ImageTexture.create_from_image(Image.load_from_file("res://assets/ressources/eggs.png"))}, "wood":{"name":"wood","texture": ImageTexture.create_from_image(Image.load_from_file("res://assets/ressources/log.png"))}}
 var components = {"tadpole":{"name":"tadpole","texture": ImageTexture.create_from_image(Image.load_from_file("res://assets/ressources/tadpole.png"))}, "plank":{"name":"plank","texture": ImageTexture.create_from_image(Image.load_from_file("res://assets/ressources/planks.png"))}}
 var forges = {"egg":{"texture":ImageTexture.create_from_image(Image.load_from_file("res://assets/machine/machine-egg_to_tadp.png"))}, "wood":{"texture":ImageTexture.create_from_image(Image.load_from_file("res://assets/machine/machine-log_to_planck.png"))}}
@@ -17,8 +18,7 @@ var stock_dict = {"tadpole": 0, "plank": 0}
 # Add amount
 func add_stock_amount(name, value):
 	stock_dict[name] += value
-	emit_signal("stock_amount_changed")
-
+	emit_signal("stock_amount_changed")	
 
 # Remove amount
 func remove_stock_amount(name, value):
@@ -32,5 +32,8 @@ func get_stock_amount(name):
 # Getter amount
 func emit_item_selected_buyzone(index):
 	emit_signal("item_selected_buyzone", index)
+	
+func emit_remove_placed_tile(event):
+	emit_signal("remove_placed_tile", event)
 
 var wave = 1
